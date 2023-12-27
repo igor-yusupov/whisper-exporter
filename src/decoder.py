29a24cd59e7d7ce44dfbeac3cb6e279d5ec01a76
Igor Yusupov <i.yusupov@cdnvideo.com>
2023-12-27 23:06:47 +0500

    Remove AudioEncoder from decoder file

--- src/decoder.py
+++ src/decoder.py
@@ -165,43 +165,6 @@ class ResidualAttentionBlock(nn.Module):
         return x, k, v
 
 
-class AudioEncoder(nn.Module):
-    def __init__(
-        self, n_mels: int, n_ctx: int, n_state: int, n_head: int, n_layer: int
-    ):
-        super().__init__()
-        self.conv1 = nn.Conv1d(n_mels, n_state, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv1d(
-            n_state, n_state, kernel_size=3, stride=2, padding=1
-        )
-        self.register_buffer("positional_embedding", sinusoids(n_ctx, n_state))
-
-        self.blocks = nn.ModuleList(
-            [
-                ResidualAttentionBlock(n_ctx, n_state, n_head)
-                for _ in range(n_layer)
-            ]
-        )
-        self.ln_post = nn.LayerNorm(n_state)
-
-    def forward(self, x: Tensor):
-        """
-        x : torch.Tensor, shape = (batch_size, n_mels, n_ctx)
-            the mel spectrogram of the audio
-        """
-        x = F.gelu(self.conv1(x))
-        x = F.gelu(self.conv2(x))
-        x = x.permute(0, 2, 1)
-
-        x = (x + self.positional_embedding[: x.shape[1]]).to(x.dtype)
-
-        for block in self.blocks:
-            x = block(x)
-
-        x = self.ln_post(x)
-        return x
-
-
 class TextDecoder(nn.Module):
     def __init__(
         self, n_vocab: int, n_ctx: int, n_state: int, n_head: int, n_layer: int
